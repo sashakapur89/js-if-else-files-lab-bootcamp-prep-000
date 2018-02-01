d5039c52b44fac360bafec9705fa3798d2f11f8c
Sasha Kapur <sashakapur89@gmail.com>
2018-02-01 01:15:17 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,9 +1,9 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name === "Joe") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Joe, Received: "+index.name)
 }
 
 
